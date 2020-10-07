dd723e8aafb583fa5fe3a4876519e820cfc26f3c
Jamie <jamiejoyce01@gmail.com>
2020-10-07 09:52:48 +0100

    Q8

---
+++ Q8.rb
@@ -0,0 +1,72 @@
+require 'date'
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = "jeff"
+  while !name.empty? do
+    # get another name from the user
+    puts "enter name"
+    name = gets.chomp
+    break if name.empty?
+    while true do
+      puts "enter cohort"
+      input_cohort = gets.chomp.capitalize
+      if input_cohort == ""
+        input_cohort = "November"
+        break
+      elsif !Date::MONTHNAMES.include?(input_cohort)
+        puts "invalid input"
+      else
+        break
+      end
+    end
+    # add the student hash to the array
+    students << {name: name, cohort: input_cohort.to_sym}
+    puts "Now we have #{students.count} students"
+
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]}"+ " "*(25-student[:name].length) +"(#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+def print_cohort(students)
+  cohorts = {}
+  students.each do |key|
+    name = key[:name]
+    cohort = key[:cohort]
+    if cohorts[cohort] == nil
+      cohorts[cohort] = [name]
+    else
+      cohorts[cohort].push(name)
+    end
+  end
+  cohorts.each do |key, value|
+    puts key
+    puts value
+  end
+end
+
+students = input_students
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
+print_cohort(students)
